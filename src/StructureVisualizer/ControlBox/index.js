e211224eb415f601eea243511e46d5643a9e4a12
Sharan poojari <136159249+SharanRP@users.noreply.github.com>
2024-03-06 13:04:15 +0000

    bootstrap implemented

--- src/StructureVisualizer/ControlBox/index.js
+++ src/StructureVisualizer/ControlBox/index.js
@@ -1,6 +1,7 @@
 import React from "react";
-
 import "./index.css";
+
+import { Row, Col, Form, Button, FormGroup } from "react-bootstrap";
 
 class ControlBox extends React.Component {
   constructor(props) {
@@ -24,68 +25,75 @@ class ControlBox extends React.Component {
   render() {
     return (
       <div className="control-box">
-        <div className="control-box-row">
-          <div className="supercell-container">
-            <label>Supercell: </label>
-            {[0, 1, 2].map((index) => (
-              <input
-                key={index}
-                className="supercell-input"
-                type="number"
-                min="1"
-                value={this.props.viewerParams.supercell[index]}
-                onChange={(e) =>
-                  this.handleSupercellChange(index, e.target.value)
-                }
+        <Row className="control-box-row">
+          <Col>
+            <Form.Label className="control-box-row-label">Supercell: </Form.Label>
+            <Row>
+              {[0, 1, 2].map((index) => (
+                <Col xs={4} key={index}>
+                  <Form.Control
+                    type="number"
+                    min="1"
+                    value={this.props.viewerParams.supercell[index]}
+                    onChange={(e) =>
+                      this.handleSupercellChange(index, e.target.value)
+                    }
+                    className="supercell-input"
+                  />
+                </Col>
+              ))}
+            </Row>
+          </Col>
+          <Col>
+            <Form.Label className="control-box-row-label">Camera: </Form.Label>
+            <div className="camera-controls">
+              <Button onClick={() => this.handleCameraEvent("x")} className="control-box-row button">
+                X
+              </Button>
+              <Button onClick={() => this.handleCameraEvent("y")} className="control-box-row button">
+                Y
+              </Button>
+              <Button onClick={() => this.handleCameraEvent("z")} className="control-box-row button">
+                Z
+              </Button>
+            </div>
+          </Col>
+        </Row>
+        <Row className="control-box-row">
+          <Col>
+            <Form.Label className="control-box-row-label">Options:</Form.Label>
+            <FormGroup>
+              <Form.Check
+                type="checkbox"
+                label="Bonds"
+                checked={this.props.viewerParams.bonds}
+                onChange={() => this.handleOptionChange("bonds")}
+                className="control-box-row-checkbox"
               />
-            ))}
-          </div>
-          <div className="camera-controls">
-            <label>Camera: </label>
-            <button onClick={() => this.handleCameraEvent("x")}>x</button>
-            <button onClick={() => this.handleCameraEvent("y")}>y</button>
-            <button onClick={() => this.handleCameraEvent("z")}>z</button>
-          </div>
-        </div>
-        <div className="control-box-row">
-          <label>
-            <input
-              className="option-checkbox"
-              type="checkbox"
-              checked={this.props.viewerParams.bonds}
-              onChange={() => this.handleOptionChange("bonds")}
-            />
-            Bonds
-          </label>
-          <label>
-            <input
-              className="option-checkbox"
-              type="checkbox"
-              checked={this.props.viewerParams.atomLabels}
-              onChange={() => this.handleOptionChange("atomLabels")}
-            />
-            Atom labels
-          </label>
-          <label>
-            <input
-              className="option-checkbox"
-              type="checkbox"
-              checked={this.props.viewerParams.packedCell}
-              onChange={() => this.handleOptionChange("packedCell")}
-              disabled={true}
-            />
-            Packed cell
-          </label>
-          <label>
-            <input
-              className="option-checkbox"
-              type="checkbox"
-              checked={this.props.viewerParams.spaceFilling}
-              onChange={() => this.handleOptionChange("vdwRadius")}
-            />
-            vdW radius
-          </label>
-        </div>
+              <Form.Check
+                type="checkbox"
+                label="Atom labels"
+                checked={this.props.viewerParams.atomLabels}
+                onChange={() => this.handleOptionChange("atomLabels")}
+                className="control-box-row-checkbox"
+              />
+              <Form.Check
+                type="checkbox"
+                label="Packed cell"
+                checked={this.props.viewerParams.packedCell}
+                disabled
+                className="control-box-row-checkbox"
+              />
+              <Form.Check
+                type="checkbox"
+                label="vdW radius"
+                checked={this.props.viewerParams.spaceFilling}
+                onChange={() => this.handleOptionChange("vdwRadius")}
+                className="control-box-row-checkbox"
+              />
+            </FormGroup>
+          </Col>
+        </Row>
       </div>
     );
   }
